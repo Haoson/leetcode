9bbccafb757b299a0dc206847c4a3d8636767671
Haoson <haoweiqinghaoson@gmail.com>
2015-03-09 11:10:12 -0700

    add integer_to_roman solution

---
+++ LeetCode/pass/integer_to_roman.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+  > File Name: integer_to_roman.cpp
+  > Author:Haoson 
+  > Created Time: Mon 09 Mar 2015 10:59:29 AM PDT
+  > Description: 
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string str;  
+        string symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};  
+        int value[]=    {1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1}; 
+        for(int i=0;num!=0;++i){
+            while(num>=value[i]){
+                num-=value[i];
+                str+=symbol[i];
+            }
+        }
+        return str;
+    }
+};
